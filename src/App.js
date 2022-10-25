c70f12685432ba01a73a8e47afdc0b54868c5a4f
GuilhermeTGG <guilherme050592@gmail.com>
2022-10-24 21:31:25 -0300

    Andamento

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ export default () => {
       setMovieList(list);
 
       //pegando o fetarued
-      
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen]; 
+      let chonsenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chonsenInfo);
     }
 
     loadAll();
@@ -25,13 +29,13 @@ export default () => {
   return (
     <div className="page">
 
-     {featuredData &&
-      <FeaturedMovie item={featuredData}/>
-     }
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
 
       <section className="lists">
         {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items}/>
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
